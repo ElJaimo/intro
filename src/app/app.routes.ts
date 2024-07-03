a77001cd4f83304dcacc2df58735df3f6c2a3462
El Jaimo <hawk_1036@live.com.mx>
2024-07-02 19:16:52 -0600

    Renamed home to main component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
     {
-        path: 'home',
-        component: HomeComponent
+        path: '',
+        component: MainComponent
     },
     {
         path: '**',
