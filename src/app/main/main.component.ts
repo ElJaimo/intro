f69ce6e94de3614f278d639dd45e3332d7c8e517
El Jaimo <hawk_1036@live.com.mx>
2024-07-06 13:34:18 -0600

    Updated first section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,9 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
+  standalone: true,
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  networksLinks = [
+    {
+      id: 'linkedin',
+      icon: '',
+      title: 'Linkedin',
+      link: '',
+    },
+    {
+      id: 'github',
+      icon: '',
+      title: 'Github',
+      link: '',
+    },
+    {
+      id: 'mail',
+      icon: '',
+      title: 'Mail me',
+      link: '',
+    },
+  ];
   constructor() {}
 
   ngOnInit() {}
