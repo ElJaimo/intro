0b3faf0636b734f6929568015c95bbcd45827d73
El Jaimo <hawk_1036@live.com.mx>
2024-07-14 19:29:13 -0600

    Added courses typescript data

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,7 @@ import { NavbarComponent } from '../navbar/navbar.component';
   styleUrls: ['./main-hero.css', 'about-me.css', 'skills.css', 'courses.css'],
 })
 export class MainComponent implements OnInit {
+  letter = 'o';
   networksLinks = [
     {
       id: 'linkedin',
@@ -48,7 +49,50 @@ export class MainComponent implements OnInit {
     'Python',
     'SQL',
   ];
-  letter = 'o';
+  courses = [
+    {
+      title: 'Análisis de Datos de Google',
+      date: 'Jan, 2024',
+      source: 'Google',
+      icon: 'data-icon.png',
+      link: 'https://coursera.org/share/93f5a57a76c216035c35fc7b4df7ac37',
+    },
+    {
+      title: 'Getting Started with Front-End and Web Development',
+      date: 'Apr, 2024',
+      source: 'IBM',
+      icon: 'frontend-icon.png',
+      link: 'https://coursera.org/share/85906fb6cbaa1f058187b04b2e4eda8a',
+    },
+    {
+      title: 'Introduction to Software Engineering',
+      date: 'Apr, 2024',
+      source: 'IBM',
+      icon: 'engineering-icon.png',
+      link: 'https://coursera.org/share/21c7cd66ae1b68105f8005f694d91c77',
+    },
+    {
+      title: 'Designing User Interfaces and Experiences (UI/UX)',
+      date: 'May, 2024',
+      source: 'IBM',
+      icon: 'uiux-icon.png',
+      link: 'https://coursera.org/share/70392a53f661e269e351483be02ae606',
+    },
+    {
+      title: 'Introduction to HTML, CSS, & JavaScript',
+      date: 'Jun, 2024',
+      source: 'IBM',
+      icon: 'html-css-js-icon.png',
+      link: 'https://coursera.org/share/e95cf6ffe4b61b486fa4d781a9525ad8',
+    },
+    {
+      title: 'Developing Websites and Front-Ends with Bootstrap',
+      date: 'Jun, 2024',
+      source: 'IBM',
+      icon: 'bootstrap-icon.png',
+      link: 'https://coursera.org/share/06b5bfd3e9a80f17ad0cce8e3264e345',
+    },
+  ];
 
   goToLink(url: string, newPage = true): void {
     window.open(url, newPage ? '_blank' : '_self');
