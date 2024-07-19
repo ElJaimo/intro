014f1987172c3a75ffafdf9cf7eed1daa82da3f1
El Jaimo <hawk_1036@live.com.mx>
2024-07-19 12:21:46 -0600

    Updated courses section

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-main',
@@ -55,6 +56,7 @@ export class MainComponent implements OnInit {
       date: 'Jan, 2024',
       source: 'Google',
       icon: 'data-icon.png',
+      iconWidth: 'w-20',
       link: 'https://coursera.org/share/93f5a57a76c216035c35fc7b4df7ac37',
     },
     {
@@ -62,6 +64,7 @@ export class MainComponent implements OnInit {
       date: 'Apr, 2024',
       source: 'IBM',
       icon: 'frontend-icon.png',
+      iconWidth: 'w-20',
       link: 'https://coursera.org/share/85906fb6cbaa1f058187b04b2e4eda8a',
     },
     {
@@ -69,6 +72,7 @@ export class MainComponent implements OnInit {
       date: 'Apr, 2024',
       source: 'IBM',
       icon: 'engineering-icon.png',
+      iconWidth: 'w-20',
       link: 'https://coursera.org/share/21c7cd66ae1b68105f8005f694d91c77',
     },
     {
@@ -76,6 +80,7 @@ export class MainComponent implements OnInit {
       date: 'May, 2024',
       source: 'IBM',
       icon: 'uiux-icon.png',
+      iconWidth: 'w-12',
       link: 'https://coursera.org/share/70392a53f661e269e351483be02ae606',
     },
     {
@@ -83,6 +88,7 @@ export class MainComponent implements OnInit {
       date: 'Jun, 2024',
       source: 'IBM',
       icon: 'html-css-js-icon.png',
+      iconWidth: 'w-16',
       link: 'https://coursera.org/share/e95cf6ffe4b61b486fa4d781a9525ad8',
     },
     {
@@ -90,6 +96,7 @@ export class MainComponent implements OnInit {
       date: 'Jun, 2024',
       source: 'IBM',
       icon: 'bootstrap-icon.png',
+      iconWidth: 'w-20',
       link: 'https://coursera.org/share/06b5bfd3e9a80f17ad0cce8e3264e345',
     },
     {
@@ -97,9 +104,11 @@ export class MainComponent implements OnInit {
       date: 'Jul, 2024',
       source: 'IBM',
       icon: 'git-icon.png',
+      iconWidth: 'w-20',
       link: 'https://coursera.org/share/395b93f5624677d13070fffd63661de5',
     },
   ];
+  activeCourse = 0;
 
   goToLink(url: string, newPage = true): void {
     window.open(url, newPage ? '_blank' : '_self');
