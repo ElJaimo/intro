64dfe0accbe5e815582000d1a678990aff09bf93
El Jaimo <hawk_1036@live.com.mx>
2025-02-15 11:08:45 -0600

    fix / Adjustments in skills section for mobile screen

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -50,7 +50,8 @@ export class MainComponent implements OnInit, AfterViewInit, OnDestroy {
   ngAfterViewInit(): void {
     document.getElementById('type-out-content')?.classList.add('typing');
     setTimeout(() => (this.letter = 'e'), 1350);
-    if (this.SkillsSection) {
+    console.log()
+    if (this.SkillsSection && window.innerWidth > 425) {
       this.intersectionObserverService.observe(
         this.SkillsSection.nativeElement
       );
